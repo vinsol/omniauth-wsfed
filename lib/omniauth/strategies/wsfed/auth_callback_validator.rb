4ff4d146af1c29f1c22b2aadf041a661d2b70c16
Jitendra <jitendra.vinsol@gmail.com>
2015-10-23 18:26:16 +0530

    fixes for saml 2 parsing

--- lib/omniauth/strategies/wsfed/auth_callback_validator.rb
+++ lib/omniauth/strategies/wsfed/auth_callback_validator.rb
@@ -33,8 +33,10 @@ module OmniAuth
         end
 
         def validate_audience!
+          audience = auth_callback.audience.ends_with?('/') ? auth_callback.audience : auth_callback.audience + '/'
+          realm = wsfed_settings[:realm].ends_with?('/') ? wsfed_settings[:realm] : wsfed_settings[:realm] + '/'
           raise OmniAuth::Strategies::WSFed::ValidationError.new(AUDIENCE_MISMATCH) unless
-              auth_callback.audience == wsfed_settings[:realm]
+              audience == realm
         end
 
         def validate_token_expiration!
